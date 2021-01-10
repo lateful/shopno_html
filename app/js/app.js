05594d2cd322ad0fba22d116fd030e18ce9f16f6
lateful <artika0005@gmail.com>
2021-01-10 17:17:31 +0300

    1.0.1

---
+++ app/js/app.js
@@ -0,0 +1,9 @@
+import $ from 'jquery'; window.jQuery = $; window.$ = $ // import jQuery module (npm i -D jquery)
+import lightbox from 'lightbox2'; window.jQuery = $; window.$ = $
+
+// require('~/app/libs/mmenu/js/jquery.mmenu.all.min.js') // import vendor jQuery plugin example (not module)
+
+document.addEventListener('DOMContentLoaded', () => {
+
+	// Custom JS
+})
